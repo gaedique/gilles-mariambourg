24f25f7414d7838290ae167931adeae9bba20d12
gaedique <gaedique4@gmail.com>
2025-01-31 10:33:44 +0100

    Création de la section Actualité

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import About from "@/src/components/homePage/About";
 import Hero from "@/src/components/homePage/Hero";
 import Layout from "@/src/components/layout/Layout";
 import Surgery from "@/src/components/homePage/Surgery";
+import News from "@/src/components/homePage/News";
 
 export default function Home() {
   return (
     <>
       <Layout>
         <Hero />
+        <News />
         <About />
         <Surgery />
       </Layout>
